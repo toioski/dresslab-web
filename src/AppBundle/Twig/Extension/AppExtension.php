d92f736f2d349b78b16ad29ba663423f0832f667
toioski <toioski@hotmail.it>
2016-05-03 11:14:39 +0200

    creata estensione gulp symony

---
+++ src/AppBundle/Twig/Extension/AppExtension.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AppBundle\Twig\Extension;
+
+class AppBundleExtension extends \Twig_Extension
+{
+    private $container;
+    private $gulp_rev_manifest;
+    
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName() {
+        return 'twig.extension.apptwig';
+    }
+    
+    public function __construct($container) {
+        $this->container = $container;
+        
+        // Gulp manifest
+        $this->gulp_rev_manifest = [];
+        $app_dir = $container->get('kernel')->getRootDir();
+        $manifestPath = $app_dir . '/Resources/assets/rev-manifest.json';
+
+        if (file_exists($manifestPath)) {
+            $this->gulp_rev_manifest = json_decode(file_get_contents($manifestPath), TRUE);
+        }
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function getFilters() {
+        return array(
+            'gulp_asset_version' => new \Twig_Filter_Method($this, 'gulpAssetVersion')
+        );
+    }
+
+    public function gulpAssetVersion($filename) {
+        if (array_key_exists($filename, $this->gulp_rev_manifest)) {
+            return $this->gulp_rev_manifest[$filename];
+        } else {
+            return $filename;
+        }
+    }
+    
+}
