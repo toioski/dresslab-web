14f92b30b08a25424b33ed6194a79c37fc23de69
Ilario Pierbattista <pierbattista.ilario@gmail.com>
2016-05-04 16:47:48 +0200

    aggiunta immagini

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,9 +2,12 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Articolo;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DefaultController extends BaseController
 {
@@ -17,5 +20,47 @@ class DefaultController extends BaseController
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
+    }
+
+    /**
+     * @param $id
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @Route("/articolo/{id}", name="app_articolo_get")
+     */
+    public function getArticoloAction($id) {
+        $em = $this->getDoctrine()->getManager();
+        /** @var Articolo $articolo */
+        $articolo = $em->getRepository("AppBundle:Articolo")
+            ->find($id);
+        return $this->jsonResponse($this->serialize($articolo));
+    }
+
+    /**
+     * @param $id
+     *
+     * @return Response
+     *
+     * @Route("/articolo/{id}/image", name="app_articolo_image_get")
+     */
+    public function getImmagineArticolo($id) {
+        $imagePath = $this->get('kernel')->getRootDir() . DIRECTORY_SEPARATOR
+            . "Resources" . DIRECTORY_SEPARATOR
+            . "assets" . DIRECTORY_SEPARATOR
+            . "images" . DIRECTORY_SEPARATOR
+            . "vestiti";
+
+        $filepath = realpath($imagePath . DIRECTORY_SEPARATOR . "capo_{$id}.jpg");
+        if(! file_exists($filepath)) {
+            throw new NotFoundHttpException();
+        }
+
+        $content = file_get_contents($filepath);
+
+        $response = new Response();
+        $response->headers->set("Content-Type", "image/jpg");
+        $response->setContent($content);
+        return $response;
     }
 }
