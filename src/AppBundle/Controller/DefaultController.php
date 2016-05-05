cc0898c809ee20e2c2fc607674ebbdcf010f00ee
Ilario Pierbattista <pierbattista.ilario@gmail.com>
2016-05-05 09:45:58 +0200

    base 64

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -59,7 +59,7 @@ class DefaultController extends BaseController
 
         $response = new Response();
         $response->headers->set("Content-Type", "image/jpg");
-        $response->setContent(base64_encode($content));
+        $response->setContent($content);
         return $response;
     }
 }
